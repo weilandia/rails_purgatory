15077477095283022397b5b52d983e7ee9003fc6
Nick Weiland <nickweiland@gmail.com>
2016-04-22 17:59:56 -0600

    add third exercise

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,6 @@ class Exercise < ActiveRecord::Base
   has_many :user_exercises
   before_save :encode_exercise
   before_save :encode_solution_frame
-  before_create :add_level
 
   def encode_exercise
     self.encoded_spec = Base64.encode64(spec)
@@ -14,9 +13,5 @@ class Exercise < ActiveRecord::Base
 
   def parse_name
     spec_path.gsub("app/purgatory_challenges/_spec/","").gsub("_spec.rb","")
-  end
-
-  def add_level
-    self.level = Exercise.count
   end
 end
