12574318edaed6b53260bc0c878c6c8ff74201ed
Nick Weiland <nickweiland@gmail.com>
2016-04-25 13:41:24 -0600

    fix heroku

--- app/controllers/purgatories_controller.rb
+++ app/controllers/purgatories_controller.rb
@@ -2,9 +2,7 @@ class PurgatoriesController < ApplicationController
   def create
     service = GithubService.new(current_user)
     service.create_purgatory
-    if service.purgatory?
-      service.create_first_exercise
-    end
+    service.create_first_exercise
     redirect_to user_path(current_user.nickname)
   end
 end
